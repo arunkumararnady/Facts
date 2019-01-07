eae28b0306a6c9556f22122d4e8f5877183d56d2
Aruna Kumara <aruna.kumara@diona.com>
2019-01-07 10:26:54 +0530

    Cretae models

---
+++ app/src/main/java/com/assignment/facts/data/CountryData.java
@@ -0,0 +1,34 @@
+package com.assignment.facts.data;
+
+import java.util.List;
+
+/**
+ * An object representing the response.
+ */
+public class CountryData {
+    private String title;
+    private List<RowData> rows;
+
+    public CountryData(String title, List<RowData> rows) {
+        this.title = title;
+        this.rows = rows;
+    }
+
+    /**
+     * Get the title.
+     *
+     * @return The title.
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Get the rows data.
+     *
+     * @return The rows.
+     */
+    public List<RowData> getRowsData() {
+        return rows;
+    }
+}
---
+++ app/src/main/java/com/assignment/facts/data/RowData.java
@@ -0,0 +1,47 @@
+package com.assignment.facts.data;
+
+/**
+ * An object representing a row.
+ */
+public class RowData {
+
+    private String title;
+    private String description;
+    private String imageHref;
+
+    public RowData(String title, String description, String imageHref) {
+        this.title = title;
+        this.description = description;
+        this.imageHref = imageHref;
+    }
+
+    /**
+     * Get the url of the image.
+     *
+     * @return The url.
+     */
+    public String getImageURL() {
+        return imageHref;
+    }
+
+    /**
+     * Get the title for the row.
+     *
+     * @return The title.
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Get the description for the row.
+     *
+     * @return The description.
+     */
+    public String getDescription() {
+        if (description == null)
+            return "";
+        return description;
+    }
+
+}
