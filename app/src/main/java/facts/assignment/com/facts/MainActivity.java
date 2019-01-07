7b8e934baf73ab153145b879b5c00a50ac399ef7
Aruna Kumara <aruna.kumara@diona.com>
2019-01-07 09:37:12 +0530

    Initial version

---
+++ app/src/main/java/facts/assignment/com/facts/MainActivity.java
@@ -0,0 +1,13 @@
+package facts.assignment.com.facts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
