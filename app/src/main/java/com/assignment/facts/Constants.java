83f04c2e88586fc1b5f7f4947d0d5fd0bce2e073
Aruna Kumara <aruna.kumara@diona.com>
2019-01-07 11:20:37 +0530

    Add constants file

---
+++ app/src/main/java/com/assignment/facts/Constants.java
@@ -0,0 +1,13 @@
+package com.assignment.facts;
+
+/**
+ * Constant values for the PixFinder Application.
+ */
+public class Constants {
+
+    public static String BASE_URL = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/";
+
+    public static String getBaseUrl() {
+        return BASE_URL;
+    }
+}
--- app/src/main/java/com/assignment/facts/repository/PixService.java
+++ app/src/main/java/com/assignment/facts/repository/PixService.java
@@ -1,5 +1,7 @@
 package com.assignment.facts.repository;
 
+import com.assignment.facts.Constants;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -11,7 +13,7 @@ public class PixService {
     public static ServiceApi createService() {
         Retrofit.Builder builder = new Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl("https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/");
+                .baseUrl(Constants.getBaseUrl());
 
         return builder.build().create(ServiceApi.class);
     }
